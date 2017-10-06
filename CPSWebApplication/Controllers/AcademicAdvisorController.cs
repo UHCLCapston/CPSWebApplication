2ac009faf71dbfbad96f88a533ad93e25765852c
kumudini jadhav <jadhavkumudini@yahoo.in>
2017-10-06 09:01:17 -0500

    Commit on 10/6/17_9AM

---
+++ CPSWebApplication/Controllers/AcademicAdvisorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CPSWebApplication.Controllers
+{
+    public class AcademicAdvisorController : Controller
+    {
+        // GET: AcademicAdvisor
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
